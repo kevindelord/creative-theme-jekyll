44e54f39141989d8bab22efc8775978fdb2bfef5
Kevin Delord <delord.kevin@gmail.com>
2025-01-12 10:57:07 +0100

    localize 404 page

--- js/language-redirect.js
+++ js/language-redirect.js
@@ -2,16 +2,9 @@
  * Custom Javascript to redirect user based on language settings.
  */
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Get browser language
-  const userLang = navigator.language || navigator.userLanguage;
-  const langCode = userLang.split('-')[0]; // Extract primary language code (e.g., 'en' from 'en-US')
+import { currentLanguage } from '/js/getCurrentLanguage.js';
 
-  // Define supported languages and their paths
-  // Fallback to 'fr' if no match
-  const supportedLanguages = ['fr', 'en', 'de'];
-  const lang = (supportedLanguages.includes(langCode) ? langCode : 'fr')
-  
+document.addEventListener("DOMContentLoaded", function() {
   // Get current path
   const currentPath = window.location.pathname;
   // Check if the user is on the base URL (root path)
@@ -19,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Redirect only if on the base URL and not already on a localized URL
   if (isBaseURL) {
-      window.location.href = `/${lang}/`;
+    const lang = currentLanguage()
+    window.location.href = `/${lang}/`;
   }
 });
